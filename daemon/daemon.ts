6823484c96d9ef867f33b84585de34da75689d24
Shuji Koike <shuji.koike@me.com>
2020-09-06 15:47:08 +0900

    fixup! [ci skip] WIP WIP WIP WIP WIP

---
+++ daemon/daemon.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import { getState } from "./state"
+import { createProxyServer } from "http-proxy"
+import execa from "execa"
+
+const proxy = createProxyServer({
+  ws: true,
+  xfwd: true,
+  changeOrigin: true,
+  autoRewrite: true,
+  timeout: 3e3,
+})
+proxy.on("error", e => console.error(e))
+
+export function listen(host: string, port: number) {
+  const app = express()
+  app.set("json spaces", 2)
+  app.get("/.bozz", (req, res) => {
+    console.info(req.url)
+    res.send(getState())
+  })
+  app.use((req, res) => {
+    const target = resolveProxyTarget(req.url)
+    console.debug("proxy:", target, req.url)
+    proxy.web(req, res, { target }, error => {
+      console.warn(error.message)
+      res.send("error")
+    })
+  })
+  const server = app.listen(port, host)
+  process.on("SIGTERM", () => server.close())
+  console.info("listen:", `http://${host}:${port}/`)
+}
+
+function resolveProxyTarget(url: string) {
+  return "http://127.0.0.1:3000/"
+}
+
+export function run(command: string, args: string[] = [], cwd?: string) {
+  const proc = execa(command, args, {
+    cwd,
+    detached: true,
+  })
+  // proc.stdout && proc.stdout.pipe(process.stdout)
+  // proc.stderr && proc.stderr.pipe(process.stderr)
+  process.on("SIGTERM", () => proc.kill("SIGTERM"))
+}
--- daemon/index.ts
+++ daemon/index.ts
@@ -1,19 +1,8 @@
-import express from "express"
 import config from "../config"
-import { initState, getState } from "./state"
-import { createProxyServer } from "http-proxy"
-import execa from "execa"
+import { initState } from "./state"
+import { listen } from "./daemon"
 
 let booted = false
-
-const proxy = createProxyServer({
-  ws: true,
-  xfwd: true,
-  changeOrigin: true,
-  autoRewrite: true,
-  timeout: 3e3,
-})
-proxy.on("error", e => console.error(e))
 
 setTimeout(async function boot() {
   console.info("daemon: boot")
@@ -28,37 +17,3 @@ setTimeout(async function boot() {
     return []
   }
 })
-
-function listen(host: string, port: number) {
-  const app = express()
-  app.set("json spaces", 2)
-  app.get("/.bozz", (req, res) => {
-    console.info(req.url)
-    res.send(getState())
-  })
-  app.use((req, res) => {
-    const target = resolveProxyTarget(req.url)
-    console.debug("proxy:", target, req.url)
-    proxy.web(req, res, { target }, error => {
-      console.warn(error.message)
-      res.send("error")
-    })
-  })
-  const server = app.listen(port, host)
-  process.on("SIGTERM", () => server.close())
-  console.info("listen:", `http://${host}:${port}/`)
-}
-
-function resolveProxyTarget(url: string) {
-  return "http://127.0.0.1:3000/"
-}
-
-function run(command: string, args: string[] = [], cwd?: string) {
-  const proc = execa(command, args, {
-    cwd,
-    detached: true,
-  })
-  // proc.stdout && proc.stdout.pipe(process.stdout)
-  // proc.stderr && proc.stderr.pipe(process.stderr)
-  process.on("SIGTERM", () => proc.kill("SIGTERM"))
-}
