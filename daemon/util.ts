65fc030a9ad9efc28c7689de56c5c94d14588cdb
Shuji Koike <shuji.koike@me.com>
2020-09-12 16:12:44 +0900

    fixup! [ci skip] WIP WIP WIP WIP WIP

--- daemon/util.ts
+++ daemon/util.ts
@@ -6,10 +6,12 @@ export function run(
   args: string[] = [],
   { cwd = "..", detached, ...options }: SpawnOptionsWithoutStdio = {}
 ) {
+  // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
   const proc = spawn(command, args, {
     cwd,
     detached,
     stdio: detached ? ["ignore", openSyncLog(), openSyncLog()] : "inherit",
+    windowsHide: true,
     ...options,
   })
   if (detached) proc.unref()
